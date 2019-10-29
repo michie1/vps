d0bd2095a968ac8ffec0e34db1c830cbd8b1ae29
Michiel Vos <git@fastfox.nl>
2019-10-29 22:20:54 +0100

    nb.msvos.nl

--- web/cert/create.sh
+++ web/cert/create.sh
@@ -1,1 +1,2 @@
-certbot certonly --manual --preferred-challenges=dns --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.msvos.nl -d msvos.nl
+#certbot certonly --manual --preferred-challenges=dns --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.msvos.nl -d msvos.nl
+certbot certonly --webroot -w /usr/share/nginx/html -d msvos.nl -d sp.msvos.nl -d tt.msvos.nl -d wt.msvos.nl -d nb.msvos.nl
