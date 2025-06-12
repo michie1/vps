66954b89ecfce5ce25ded8f7ed62a01460877e05
Michiel Vos <git@fastfox.nl>
2025-06-12 22:26:13 +0200

    Update

--- web/cert/create.sh
+++ web/cert/create.sh
@@ -1,2 +1,2 @@
 #certbot certonly --manual --preferred-challenges=dns --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.msvos.nl -d msvos.nl
-certbot certonly --webroot -w /usr/share/nginx/html -d msvos.nl -d sp.msvos.nl -d tt.msvos.nl -d wt.msvos.nl -d nb.msvos.nl -d gt.msvos.nl
+certbot certonly --webroot -w /usr/share/nginx/html -d msvos.nl -d sp.msvos.nl -d tt.msvos.nl -d wt.msvos.nl -d nb.msvos.nl -d gt.msvos.nl -d convert.msvos.nl
--- web/cert/install_cron.sh
+++ web/cert/install_cron.sh
@@ -1,1 +1,2 @@
 echo "0 0 1 * * certbot renew --quiet >/dev/null 2>&1 && curl https://hc-ping.com/a93a1039-cb4e-44e6-a086-ec6be3f26187 > /dev/null" | crontab
+# maybe add service nginx restart after certbow renew
